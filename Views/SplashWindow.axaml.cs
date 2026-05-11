6ec756e4666d327a95225c457732b1bea9432a77
casey-tmc97 <casey@texasmusiccafe.org>
2026-05-11 09:48:30 -0500

    feat: add SplashWindow AXAML and code-behind

--- Views/SplashWindow.axaml.cs
+++ Views/SplashWindow.axaml.cs
@@ -1,7 +1,15 @@
+using System;
 using Avalonia.Controls;
 
 namespace ShowCast.Views;
 
 public partial class SplashWindow : Window
 {
+    public SplashWindow() => InitializeComponent();
+
+    public void Report(double value, string label)
+    {
+        SplashProgressBar.Value = value;
+        SplashStatusLabel.Text  = label;
+    }
 }
